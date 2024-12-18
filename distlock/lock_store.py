9b1cde3c8d3148fc59e7df4c9887202d90e3ddb5
daniel salmon <salmond44@gmail.com>
2024-12-17 21:30:22 -0500

    Rename LockStore attributes

--- distlock/lock_store.py
+++ distlock/lock_store.py
@@ -2,31 +2,31 @@ from threading import Lock
 from typing import TypedDict
 
 
-class Locks(TypedDict):
+class Store(TypedDict):
     key: Lock
 
 
 class LockStore:
     def __init__(self):
-        self.lock = Lock()
-        self.locks = Locks()
+        self._lock = Lock()
+        self._store = Store()
 
     def __len__(self) -> int:
-        with self.lock:
-            return len(self.locks)
+        with self._lock:
+            return len(self._store)
 
     def __getitem__(self, key: str) -> Lock:
-        with self.lock:
-            return self.locks[key]
+        with self._lock:
+            return self._store[key]
 
     def __setitem__(self, key: str, value: Lock) -> None:
-        with self.lock:
-            self.locks[key] = value
+        with self._lock:
+            self._store[key] = value
 
     def __delitem__(self, key: str) -> None:
-        with self.lock:
-            del self.locks[key]
+        with self._lock:
+            del self._store[key]
 
     def __contains__(self, key: str) -> bool:
-        with self.lock:
-            return key in self.locks
+        with self._lock:
+            return key in self._store
