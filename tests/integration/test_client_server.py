8191acca790d572620c915b906010342e01718d2
daniel salmon <salmond44@gmail.com>
2025-01-12 15:49:44 -0500

    Add more integration tests

--- tests/integration/test_client_server.py
+++ tests/integration/test_client_server.py
@@ -1,12 +1,33 @@
 import subprocess
 
+import pytest
+
 from distlock import Distlock
 
 
-def test_create_lock_delete_lock(distlock_server: subprocess.Popen) -> None:
+def cleanup(distlock: Distlock, keys: list[str]) -> None:
+    for key in keys:
+        distlock.delete_lock(key)
+    lock_names = {lock.key for lock in distlock.list_locks()}
+    assert len(lock_names) == len(lock_names - set(keys))
+
+
+@pytest.mark.parametrize("key", ["key", "different key", "one more key"])
+def test_create_lock(key: str, distlock_server: subprocess.Popen) -> None:
     distlock = Distlock()
-    distlock.create_lock("key")
-    distlock.delete_lock("key")
-    locks = distlock.list_locks()
-    lock_names = {lock.key for lock in locks}
-    assert "key" not in lock_names
+    distlock.create_lock(key)
+    cleanup(distlock, keys=[key])
+
+
+@pytest.mark.parametrize(
+    "keys",
+    [
+        ["key1", "key2", "key3"],
+        ["value1", "value2", "value3"],
+    ],
+)
+def test_create_locks(keys: list[str], distlock_server: subprocess.Popen) -> None:
+    distlock = Distlock()
+    for key in keys:
+        distlock.create_lock(key)
+    cleanup(distlock, keys=keys)
