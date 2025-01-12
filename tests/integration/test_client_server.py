855d81a48884e51246798320466524a6e9c6d69c
daniel salmon <salmond44@gmail.com>
2025-01-12 17:07:52 -0500

    Refactor testing infrastructure

--- tests/integration/test_client_server.py
+++ tests/integration/test_client_server.py
@@ -1,20 +1,12 @@
-import subprocess
-
 import pytest
 
 from distlock import Distlock
 
-
-def cleanup(distlock: Distlock, keys: list[str]) -> None:
-    for key in keys:
-        distlock.delete_lock(key)
-    lock_names = {lock.key for lock in distlock.list_locks()}
-    assert len(lock_names) == len(lock_names - set(keys))
+from .conftest import cleanup
 
 
 @pytest.mark.parametrize("key", ["key", "different key", "one more key"])
-def test_create_lock(key: str, distlock_server: subprocess.Popen) -> None:
-    distlock = Distlock()
+def test_create_lock(key: str, distlock: Distlock) -> None:
     distlock.create_lock(key)
     cleanup(distlock, keys=[key])
 
@@ -26,8 +18,12 @@ def test_create_lock(key: str, distlock_server: subprocess.Popen) -> None:
         ["value1", "value2", "value3"],
     ],
 )
-def test_create_locks(keys: list[str], distlock_server: subprocess.Popen) -> None:
-    distlock = Distlock()
+def test_create_locks(keys: list[str], distlock: Distlock) -> None:
     for key in keys:
         distlock.create_lock(key)
     cleanup(distlock, keys=keys)
+
+
+def test_delete_locks(create_locks: list[str], distlock: Distlock) -> None:
+    for key in create_locks:
+        distlock.delete_lock(key)
