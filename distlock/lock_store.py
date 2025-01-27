fffc01efb22afc878ce06d31956d33634bc60fe4
daniel salmon <salmond44@gmail.com>
2025-01-26 19:34:23 -0500

    Refactor to have thread safe and non-thread safe lock stores

--- distlock/lock_store.py
+++ distlock/lock_store.py
@@ -9,6 +9,53 @@ class Store(TypedDict):
     key: Lock
 
 
+class LockStore:
+    def __init__(self):
+        self._store = Store()
+
+    def __len__(self) -> int:
+        return len(self._store)
+
+    def __getitem__(self, key: str) -> Lock:
+        return self._store[key]
+
+    def __setitem__(self, key: str, value: Lock) -> None:
+        self._store[key] = value
+
+    def __delitem__(self, key: str) -> None:
+        del self._store[key]
+
+    def __contains__(self, key: str) -> bool:
+        return key in self._store
+
+    def acquire(self, key: str, expires_in_seconds: int) -> Lock:
+        lock = self._store[key]
+        if lock.acquired and not lock.expired:
+            unacquired_lock = Lock(
+                key=lock.key,
+                acquired=False,
+                clock=lock.clock,
+                expires_at=lock.expires_at,
+            )
+            return unacquired_lock
+        lock.acquire(expires_in_seconds=expires_in_seconds)
+        return lock
+
+    def release(self, key: str, clock: int) -> None:
+        lock = self._store[key]
+        lock.release(clock=clock)
+
+    def set_not_exists(self, key: str, value: Lock) -> None:
+        if key in self._store:
+            raise AlreadyExistsError
+        self._store[key] = value
+
+    # We define this method so that the API of this class is the same as the
+    # ThreadSafeLockStore class.
+    def to_list(self) -> list[Lock]:
+        return list(self._store.values())
+
+
 class ThreadSafeLockStore:
     def __init__(self):
         self._lock = threading.Lock()
